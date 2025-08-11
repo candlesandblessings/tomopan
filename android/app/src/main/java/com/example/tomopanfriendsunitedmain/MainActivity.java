2e7dd2c72c28b3ab09856d4a98ddf7d9f27bfe93
Cosmin Miga <cosminmiga@gmail.com>
2025-08-11 13:20:34 +0100

    [dyad] add Capacitor for mobile app support

---
+++ android/app/src/main/java/com/example/tomopanfriendsunitedmain/MainActivity.java
@@ -0,0 +1,5 @@
+package com.example.tomopanfriendsunitedmain;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.tomopanfriendsunitedmain',
+  appName: 'tomopan-friends-united-main',
+  webDir: 'dist'
+};
+
+export default config;
