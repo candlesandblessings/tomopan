051d10564d349f36818283215eec0f488e2432f8
Cosmin Miga <cosminmiga@gmail.com>
2025-08-11 17:46:26 +0100

    [dyad] Fixing GitHub Pages deployment path - wrote 1 file(s)

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/tomopan-friends-united-main/',
+  base: '/tomopan/',
   server: {
     host: "::",
     port: 8080,
